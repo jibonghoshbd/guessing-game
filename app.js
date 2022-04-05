b0872917ba674a5482fb34e647b46c44264ffff8
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-05 13:11:05 +0600

    javascript done

--- app.js
+++ app.js
@@ -1,12 +1,47 @@
+// initializing some values
+let totalAttempts = 5;
+let attempts = 0;
+let totalWons = 0;
+let totallosts = 0;
+
 // finding elements
 const cardBody = document.querySelector('.card-body')
 const form = document.querySelector('form')
-const inputNumebr = document.getElementById('guessingNumber')
+const guessingNumber = document.getElementById('guessingNumber')
 const submitBtn = document.getElementById('check-result')
-const resultText = document.querySelector('.resultText')
+const resultText = document.querySelector('.resultText');
+const lostWonMessage = document.createElement("p");
 const remainingAttempts = document.querySelector('.remainingAttempts')
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
-    console.log('hello')
+    attempts++;
+    if (attempts === 5) {
+        guessingNumber.disabled = true;
+        submitBtn.disabled = true;
+    } if (attempts < 6) {
+        const guessNumber = parseInt(guessingNumber.value);
+        checkResult(guessNumber);
+        remainingAttempts.innerHTML = `Remaining attempts: ${totalAttempts - attempts
+            }`;
+    }
+    guessingNumber.value = ''
 });
+
+const checkResult = guessingNumber => {
+    const randomNumber = getRandomNumber(5);
+    if (guessingNumber === randomNumber) {
+        resultText.innerHTML = `you have won`;
+        totalWons++;
+    } else {
+        resultText.innerHTML = `you have lost; random number was: ${randomNumber}`;
+        totallosts++;
+    }
+    lostWonMessage.innerHTML = `Won: ${totalWons}, Lost: ${totallosts}`;
+    lostWonMessage.classList.add("large-text");
+    cardBody.appendChild(lostWonMessage);
+}
+
+const getRandomNumber = limit => {
+    return Math.floor(Math.random() * limit) + 1;
+}
